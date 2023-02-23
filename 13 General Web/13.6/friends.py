3e4878faad4a5edae726fcb0cb7ee4153f2bf0ab
RaccoonskY <viktoryablonskas@yandex.ru>
2023-02-23 22:15:32 +0300

    refactored friends.py

--- 13 General Web/13.6/friends.py
+++ 13 General Web/13.6/friends.py
@@ -1,39 +1,71 @@
-import collections
-import json
 import datetime
-
 import requests
 
-def calc_age(uid):
+ACCESS_TOKEN = '17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711'
+VK_API_VERSION = 5.81
+
+
+def get_user_id(uid):
+    if isinstance(uid, int):
+        return uid
+
+    user_payload = {'v': VK_API_VERSION,
+                    'access_token': ACCESS_TOKEN,
+                    'user_ids': uid}
+    user_get = requests.get('https://api.vk.com/method/users.get', params=user_payload)
+    user_id = dict(user_get.json())['response'][0]['id']
+
+    return user_id
+
+
+def get_friends(user_id):
+    friends_payload = {'v': VK_API_VERSION,
+                       'access_token': ACCESS_TOKEN,
+                       'user_id': user_id,
+                       'fields': 'bdate'}
+    friends_get = requests.get('https://api.vk.com/method/friends.get', params=friends_payload)
+    friends = dict(friends_get.json())['response']['items']
+
+    return friends
+
+
+def get_pairs(users):
     current_year = datetime.datetime.now().year
-    access_token = '92b0d2e792b0d2e792b0d2e75791a21b74992b092b0d2e7f146e8e7090b813dfa76951c'
-    parameters = {'user_ids': uid}
-    id_js_req = requests.request('GET', url=("https://api.vk.com/method/users.get?v=5.81&access_token="+access_token),params=parameters).json()
-    id = dict(id_js_req)['response'][0]['id']
-    parameters = {'user_id': id, 'fields': 'bdate'}
-    friends = requests.request('GET', url=('https://api.vk.com/method/friends.get?v=5.81&access_token='+access_token), params=parameters)
-    age_list = []
 
-    for i in friends.json()['response']['items']:
-        if i.get('bdate') != None:
-            if len(i['bdate']) > 5:
-                year = current_year - int(i['bdate'][len(i['bdate'])-4:])
-                contained = False
-                for j in age_list:
-                    if j.__contains__(year):
-                        j[1] +=1
-                        contained = True
-                    if contained:
-                        break
-                if not contained:
-                    age_list.append([year,1])
+    ages = []
+    for user in users:
+        if 'bdate' in user and len(user['bdate'].split('.')) == 3:
+            birth_year = datetime.datetime.strptime(user['bdate'], "%d.%m.%Y").year
+            age = current_year - birth_year
+            ages.append(age)
 
-    age_list.sort(key=lambda x: x[0])
-    age_list.sort(key=lambda x:x[1],reverse=True)
+    pairs = count_ages(ages)
+    return pairs
 
-    age_list = [(i[0],i[1]) for i in age_list]
-    return age_list
 
+def count_ages(ages):
+    pairs = []
+    unique_ages = set(ages)
+    for age in unique_ages:
+        person_count = ages.count(age)
+        pairs.append((age, person_count))
+
+    return pairs
+
+
+def sort_pairs(pairs):
+    sorted_by_age = sorted(pairs, key=lambda item: item[0])
+    sorted_by_count = sorted(sorted_by_age, key=lambda item: item[1], reverse=True)
+    return sorted_by_count
+
+
+def calc_age(uid):
+    user_id = get_user_id(uid)
+    friends = get_friends(user_id)
+    pairs = get_pairs(friends)
+    sorted_pairs = sort_pairs(pairs)
+
+    return sorted_pairs
 
 
 if __name__ == '__main__':
